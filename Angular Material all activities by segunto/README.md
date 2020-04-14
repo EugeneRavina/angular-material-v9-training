3698e0f6d1a6a8f25e3aecd4605f64cbb372fa4b
Bryon Kurt S. Segunto <bryonkurts@gmail.com>
2020-04-14 12:32:34 +0800

    Angular material activities and checkup exam

---
+++ Angular Material all activities by segunto/README.md
@@ -0,0 +1,11 @@
+# Angular Material All activities with check-up exam activity
+
+Outline of project:
+1. Stepper.
+2. Material Card.
+3. Dialog info that displas the user's input.
+4. Table with filter, sort, pagination, and footer.
+5. Service activity with dialog feature and drag and drop feature.
+6. Check-up exam about drag and drop "Kanban".
+
+
---
+++ Angular Material all activities by segunto/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DialogsComponent } from './dialogs/dialogs.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material/material.module';
+import {MatStepperModule} from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { UserDialogsComponent } from './user-dialogs/user-dialogs.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatTableComponent } from './mat-table/mat-table.component';
+import { MatCardComponent } from './mat-card/mat-card.component';
+import { MatStepperComponent } from './mat-stepper/mat-stepper.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { DragDialogComponent } from './drag-dialog/drag-dialog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DialogComponent,
+    DialogsComponent,
+    SidenavComponent,
+    ToolbarComponent,
+    MatTableComponent,
+    MainContentComponent,
+    UserDialogsComponent,
+    MatCardComponent,
+    MatStepperComponent,
+    DragDropComponent,
+    DragDialogComponent,
+  
+  
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    MatTableModule,
+    MatStepperModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
